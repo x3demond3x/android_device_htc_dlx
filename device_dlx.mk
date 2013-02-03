be03073fa47940d0cef8a42604a015191d6825ed
x3demond3x <Gilly336@live.com>
2013-02-03 04:31:16 -0700

    Update device_dlx.mk

--- device_dlx.mk
+++ device_dlx.mk
@@ -38,6 +38,7 @@ PRODUCT_COPY_FILES += \
 
 # HTC BT audio config
 PRODUCT_COPY_FILES += device/htc/dlx/configs/AudioBTID.csv:system/etc/AudioBTID.csv
+PRODUCT_COPY_FILES += device/htc/dlx/configs/AudioBTIDnew.csv:system/etc/AudioBTIDnew.csv
 
 # QC thermald config
 PRODUCT_COPY_FILES += device/htc/msm8960-common/configs/thermald.conf:system/etc/thermald.conf
