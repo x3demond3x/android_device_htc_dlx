1b055e76f71d33c4812a60b6bb8d3f9da2eb4631
x3demond3x <Gilly336@live.com>
2013-02-05 20:31:17 -0700

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-BASE=../../../vendor/htc/dlx/proprietary
+VENDOR=htc
+DEVICE=dlx
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  adb pull /system/$FILE $BASE/$FILE
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g'`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    adb pull /system/$FILE $BASE/$FILE
 done
 
 ./setup-makefiles.sh
