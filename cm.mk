c31cd7ae415ee38a84fd7693e12176eebe8451b3
x3demond3x <Gilly336@live.com>
2013-02-05 20:16:38 -0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,28 +1,27 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
+## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/cdma.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/htc/dlx/device_dlx.mk)
-
-# Device naming
-PRODUCT_DEVICE := dlx
-PRODUCT_NAME := cm_dlx
-PRODUCT_BRAND := htc
-PRODUCT_MODEL := Droid DNA
-PRODUCT_MANUFACTURER := htc
-
-# Set build fingerprint / ID / Product Name ect.
-
-
-# Release name
-PRODUCT_RELEASE_NAME := dlx
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
--include vendor/cm/config/common_versions.mk
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/mako/full_dlx.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := dlx
+PRODUCT_NAME := cm_dlx
+PRODUCT_BRAND := HTC
+PRODUCT_MODEL := Droid DNA
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=verizon_wwe/dlx/dlx:4.1.1/JRO03C/123160.4:user/release-keys
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
