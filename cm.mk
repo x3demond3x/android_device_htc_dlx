04691c9b9dddbce49f9777dd1da5364360718db7
x3demond3x <Gilly336@live.com>
2013-02-06 00:07:48 -0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/mako/full_dlx.mk)
+$(call inherit-product, device/htc/dlx/full_dlx.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := dlx
